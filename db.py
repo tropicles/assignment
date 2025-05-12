d6ace4cc15912b34bb4e71b3fefdf024e104470a
tropicles <147875053+tropicles@users.noreply.github.com>
2025-05-12 23:37:18 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,10 +1,10 @@
 import sqlite3
 
-# Connect to SQLite database (creates file if not exists)
+
 conn = sqlite3.connect("logs.db")
 cursor = conn.cursor()
 
-# Create table
+
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS logs (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
